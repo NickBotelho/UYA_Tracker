ecf35695db0b79a292099179b4b59d636f9d86e5
NickBotelho <nickpbotelho@gmail.com>
2022-09-04 01:12:26 -0400

    tweak

--- mongodb.py
+++ mongodb.py
@@ -275,7 +275,7 @@ class Database():
         else:
             numLogins = user['num_logins']
             logins = user['logins']
-            logins.append(now)
+            logins.push(now)
             self.collection.find_one_and_update(
                 {
                     'ip':ip
