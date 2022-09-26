b2c338a35a436a4fc28fafbc65a7679f417c0e04
NickBotelho <nickpbotelho@gmail.com>
2022-09-26 17:03:29 -0400

    v3.1.3

--- frontend/src/components/homepage.js
+++ frontend/src/components/homepage.js
@@ -17,7 +17,7 @@ else{
 }
 
 function HomePage(props){
-    console.log("v3.1.2")
+    console.log("v3.1.3")
     const isMobile = useMediaQuery({
         query: "(min-width: 10px) and (max-width: 600px)", //norm is 390x800
       });
