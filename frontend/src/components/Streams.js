5ce672e7e5d342f55200e7c7f4fdf92c6bc5df93
NickBotelho <nickpbotelho@gmail.com>
2022-11-23 23:53:52 -0500

    meta tags

--- frontend/src/components/Streams.js
+++ frontend/src/components/Streams.js
@@ -34,6 +34,23 @@ function Streams(props){
     const returnHome = () =>{
         goHome(true)
     }
+    async function getDevices(){
+        return await navigator.mediaDevices.enumerateDevices()
+    }
+    let devices = getDevices()
+    console.log(devices)
+
+    function getLocalStream() {
+        navigator.mediaDevices.getUserMedia({video: true, audio: true}).then((stream) => {
+            window.localStream = stream;
+            window.localAudio.srcObject = stream;
+            window.localAudio.autoplay = true;
+        }).catch((err) => {
+            console.error(`you got an error: ${err}`)
+        });
+    }
+    
+    getLocalStream();
     
     if (home != null){
         const redirect = "/leaderboards"
